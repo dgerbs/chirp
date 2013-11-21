49e0217543e5a96ff0d74eb209ee62f9b1862acb
dgerbs <dojoh@outlook.com>
2013-11-21 15:19:22 -0500

    Add a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Chirp::Application.routes.draw do
+  get "pages/home"
+  get "pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
