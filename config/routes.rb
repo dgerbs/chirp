7b2e1e659daf7a1e8973e8c149d394baed188ff0
dgerbs <dojoh@outlook.com>
2013-11-21 15:56:00 -0500

    Reroute home

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Chirp::Application.routes.draw do
-  get "pages/home"
   get "pages/help"
   get "pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
