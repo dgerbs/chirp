ee641ff9efcc511e181f4e092e8ee727718cdabc
dgerbs <dojoh@outlook.com>
2013-11-26 18:57:35 -0500

    format validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   validates_presence_of :name
   validates_length_of   :name, maximum: 30
   validates_presence_of :email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, with: VALID_EMAIL_REGEX
 end
