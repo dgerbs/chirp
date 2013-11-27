4b130820afb02dd6816e8dac4adbe28b2e92a72b
dgerbs <dojoh@outlook.com>
2013-11-26 19:29:15 -0500

    unique validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
 
-  validates_presence_of :name
-  validates_length_of   :name, maximum: 30
-  validates_presence_of :email
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates_format_of :email, with: VALID_EMAIL_REGEX
+  validates_presence_of   :name
+  validates_length_of     :name, maximum: 30
+  validates_presence_of   :email
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_format_of     :email, with: VALID_EMAIL_REGEX
+
+  before_save { |user| user.email = email.downcase }
 end
