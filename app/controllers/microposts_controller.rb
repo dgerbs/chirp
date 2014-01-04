7cbe93ff541d7abcb169345a636e135776c59383
dgerbs <dojoh@outlook.com>
2014-01-04 14:57:28 -0500

    Add micropost functionality

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,8 +5,21 @@ class MicropostsController < ApplicationController
   end
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_path
+    else
+      render 'pages/home'
+    end
   end
 
   def destroy
   end
+
+  protected
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
